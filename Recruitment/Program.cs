45a9e741f63aa0ed47841267e11f95cc0a949fd0
Imad Karahodza <imadahmed.karahodza@elev.ga.ntig.se>
2024-10-21 13:38:53 +0200

    nästan klart

--- Recruitment/Program.cs
+++ Recruitment/Program.cs
@@ -1,4 +1,6 @@
-﻿List<string> Recruits = ["Karma", 
+﻿List<string> Recruits = 
+[
+"Karma", 
 "Adoomy", 
 "Banan", 
 "Sayboon", 
@@ -6,7 +8,9 @@
 ];
 
 //------------------------------//
+
 List<string> Soldiers = [];
+
 //------------------------------//
 
 while (Soldiers.Count < 3)
@@ -17,12 +21,43 @@ while (Soldiers.Count < 3)
         Console.WriteLine($"[{i}] {Recruits[i]}");
     }
         Console.WriteLine("\n--Current Recruits--");
-        for (int i = 0; i <Soldiers.Count; i ++);
+        for (int i = 0; i <Soldiers.Count; i ++)
     {
         Console.WriteLine($"{Soldiers[i]}");
 
     }
+    Console.WriteLine("\nChoose a recruit:");
+    int RecruitNum = 0;
+    bool Convert = false;
+    //------------------------------//
 
+    while (Convert == false || RecruitNum < 0 || RecruitNum >= Recruits.Count) 
+    {
+        string nText = Console.ReadLine();
+        Convert = int.TryParse(nText, out RecruitNum);
 
+        if (Convert == false)
+        {
+            Console.WriteLine("Please enter a number!");
+        }
+        else if (RecruitNum < 0 || RecruitNum >= Recruits.Count)
+        {
+            Console.WriteLine("Please choose an available number!");
+        }
+
+    }
+
+    //------------------------------//
+
+    string recruit = Recruits[RecruitNum];
+    Soldiers.Add(recruit);
+    Recruits.RemoveAt(RecruitNum);
 }
+
+Console.WriteLine($"You recruited a total of {Soldiers.Count} soldiers.");
+
+Console.WriteLine("Press ENTER to quit");
+Console.ReadLine();
+
+
 
